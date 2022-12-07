fbf57c6c1d4d639b27e7597f6c7780e2de34945d
Peytonjc <35534934+Peytonjc@users.noreply.github.com>
2022-12-07 05:50:59 +0000

    Solved day 4

--- Puzzle04/solution04.py
+++ Puzzle04/solution04.py
@@ -1,5 +1,35 @@
-print('Starting Solution 3...')
+print('Starting Solution 4...')
 
 #Read input file as lines
 with open('input.txt') as inputFile:
     lines = inputFile.readlines()
+score = 0
+scoreTwo = 0
+for line in lines:
+    firstElf = line.split(',')[0]
+    secondElf = line.split(',')[1].replace('\n','')
+    fLow = int(firstElf.split('-')[0])
+    fHigh = int(firstElf.split('-')[1])
+    sLow = int(secondElf.split('-')[0])
+    sHigh = int(secondElf.split('-')[1])
+    if ((fLow <= sLow) & (fHigh >= sHigh)) | ((sLow <= fLow) & (sHigh >= fHigh)):
+        #print(line)
+        #print(str(fLow) + ' ' + str(fHigh) + ' ' + str(sLow) + ' ' + str(sHigh))
+        score = score + 1
+
+for line in lines:
+    firstElf = line.split(',')[0]
+    secondElf = line.split(',')[1].replace('\n','')
+    fLow = int(firstElf.split('-')[0])
+    fHigh = int(firstElf.split('-')[1])
+    sLow = int(secondElf.split('-')[0])
+    sHigh = int(secondElf.split('-')[1])
+    if ((fLow <= sLow) & (fHigh >= sLow)) | ((fLow <= sHigh) & (fHigh >= sHigh) | (fLow <= sLow) & (fHigh >= sHigh)) | ((sLow <= fLow) & (sHigh >= fHigh)):
+        #print(line)
+        #print(str(fLow) + ' ' + str(fHigh) + ' ' + str(sLow) + ' ' + str(sHigh))
+        scoreTwo = scoreTwo + 1
+    else:
+        print (line)
+
+print('Score for Part 1: ' + str(score))
+print('Score for Part 2: ' + str(scoreTwo))
